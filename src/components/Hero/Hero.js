0c81118f65c9c0b335e7e53e37ebc629b80780f3
Christopher Nugroho <Chrisnulis@gmail.com>
2021-08-05 12:01:28 -0400

    Fixing error

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,9 +12,9 @@ const Hero = () => (
         Personal Portfolio
       </SectionTitle>
       <SectionText>
-        A full stack engineer with decade of analytical & business experience. I take pride in striving to understand and solve business problems. I am known for delivering high quality, detailed and easy to understand results.  
+        A full stack engineer with decade of analytical & business experience. I take pride in striving to understand and solve business problems. I am known for delivering high quality, detailed and easy to understand results.
       </SectionText>
-      <Button onClick={() => window.location = 'https://google.com'}>Resume</Button>
+      <Button onClick={() => window.location = 'https://docs.google.com/document/d/1kkqrrr2w7qs3bc_99IM0OliOFN7KaPlE/edit?usp=sharing&ouid=117582958796216161566&rtpof=true&sd=true'}>Resume</Button>
     </LeftSection>
   </Section>
 );
