3d09ff00a4486737d0fd10d9705ed1fec97d1ca7
Christopher Nugroho <Chrisnulis@gmail.com>
2021-08-07 22:46:18 -0400

    Fixing Typo

--- src/components/Accomplishments/Accomplishments.js
+++ src/components/Accomplishments/Accomplishments.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import { Box, Boxes, BoxNum, BoxText } from './AccomplishmentsStyles';
 
 const data = [
   { number: 20, text: 'Total Projects'},
@@ -10,9 +10,9 @@ const data = [
   { number: 503, text: 'Cups of Coffee Consumed', }
 ];
 
-const Acomplishments = () => (
+const Accomplishments = () => (
   <Section>
-    <SectionTitle>Current Acomplishments <br />
+    <SectionTitle>Current Accomplishments <br />
     Live Update</SectionTitle>
     <Boxes>
       {data.map((card, index) => (
@@ -25,4 +25,4 @@ const Acomplishments = () => (
   </Section>
 );
 
-export default Acomplishments;
+export default Accomplishments;
